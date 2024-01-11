f0b6efd5c8547323a95d205281f6cad0c00bd35e
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-01-11 20:27:14 +0530

    adding pattern_rec() method

--- recursion.java
+++ recursion.java
@@ -10,9 +10,19 @@ public class recursion {
             return n*factorial(n-1);
         }
     }
+    static void pattern_rec(int n){
+        if(n>0){
+            pattern_rec(n-1);
+            for(int i=0; i<n; i++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
         int x=5;
         System.out.println("the value of factorial n is : "+ factorial(x));
+        pattern_rec(5);
     }
 }
