e9a0fdba5e10f7f6e0087fa8257ac510cb6dfcbf
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-01-12 11:39:45 +0530

    modifing the the ehsan.java file and fixed some bugs

--- ehsan.java
+++ ehsan.java
@@ -5,40 +5,32 @@
 /**
  * Innerehsan
  */
- class Innerehsan {
-    public static void name() {
-        System.out.println("im inside Innereshan");
-    }
-    
-}
+
 class ehs {
      
-
-    void ehs() {
-        System.out.println("hello ");
+     ehs() {
         
+       System.out.println("I am inside the constructor");
     }
-    int ehs(String name){
+     ehs(String name){
         System.out.println("good morning "+name);
-        return 1;
     }
 
     
      void playMusic() {
         System.out.println("playing music.....");
     }
-}
-public class ehsan {
+
+
 
     
 
 
     public static void main(String[] args) { 
-        ehs music = new ehs();
-        music.ehs("ehsan");
-        Innerehsan.name();
-        ehs ehs1 = new ehs();
-        ehs1.ehs();
+        ehs music = new ehs("ehsan e");
+        
+        
+        
 
         music.playMusic();
     }
